3721a3703d1a29b3848e2eced5fe09652c32dc57
stefanus ayudha <stefanus.ayudha@gmail.com>
2025-07-26 13:23:09 +0700

    fix snapping behavior

--- Library/lib/src/commonMain/kotlin/Sniffer.kt
+++ Library/lib/src/commonMain/kotlin/Sniffer.kt
@@ -1,8 +1,9 @@
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.runtime.Composable
@@ -10,50 +11,60 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.onSizeChanged
-import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toIntRect
 import io.github.stefanusayudha.spoof.lib.generated.resources.Res
 import io.github.stefanusayudha.spoof.lib.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
+import kotlin.math.absoluteValue
 
 @Composable
 fun Sniffer(content: @Composable () -> Unit) {
+    val density = LocalDensity.current
     val rootSize = remember { mutableStateOf(IntSize.Zero) }
-    val widgetPosition = remember { mutableStateOf(IntOffset.Zero) }
+    val buttonSize = remember { mutableStateOf(IntSize.Zero) }
+    val widgetPosition = remember { mutableStateOf(IntOffset((-8 * density.density).toInt(), 0)) }
 
     Box(
-        Modifier
-            .onSizeChanged {
-                rootSize.value = it
-            }
+        modifier = Modifier.onSizeChanged {
+            rootSize.value = it
+        }
     ) {
         content.invoke()
         SnifferFloatingWidget(
             modifier = Modifier
-                .padding(8.dp)
                 .systemBarsPadding()
-                .align(Alignment.TopEnd)
+                .align(Alignment.CenterEnd)
                 .offset { widgetPosition.value }
-                .draggable(
-                    state = rememberDraggableState {
-                        widgetPosition.value += IntOffset(x = 0, y = it.toInt())
-                    },
-                    orientation = Orientation.Vertical
-                )
-                .onGloballyPositioned {
-                    val rect = it.positionInRoot()
-                    val rootHeight = rootSize.value.height
-                    val topDelta = rect.y - widgetPosition.value.y
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDragEnd = {
+                            val rootRect = rootSize.value.toIntRect()
 
-                    if (widgetPosition.value.y < 0)
-                        widgetPosition.value = IntOffset(x = 0, y = 0)
-
-                    if (rect.y + topDelta > rootHeight)
-                        widgetPosition.value = IntOffset(x = 0, y = (widgetPosition.value.y - 100))
+                            if (widgetPosition.value.x.absoluteValue < rootRect.center.x - buttonSize.value.width / 2) {
+                                // snap right
+                                widgetPosition.value = widgetPosition.value
+                                    .copy(x = (-8 * this.density).toInt())
+                            } else {
+                                // snap left
+                                widgetPosition.value = widgetPosition.value
+                                    .copy(x = -1 * rootSize.value.width + buttonSize.value.width + (8 * this.density).toInt())
+                            }
+                        },
+                        onDrag = { change, dragAmount ->
+                            val finalPos =
+                                widgetPosition.value + IntOffset(x = dragAmount.x.toInt(), y = dragAmount.y.toInt())
+                            widgetPosition.value = finalPos
+                        }
+                    )
+                }
+                .onSizeChanged {
+                    buttonSize.value = it
                 },
             onClick = {
 
